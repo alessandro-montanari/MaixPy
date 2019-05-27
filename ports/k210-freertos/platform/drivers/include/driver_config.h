3dfb481c0f25079bad202b2cefca3c4f85709853
alessandro.montanari <alessandro.montanari@nokia-bell-labs.com>
2019-05-27 17:12:12 +0100

    Allow VGA resolution for camera

--- ports/k210-freertos/platform/drivers/include/driver_config.h
+++ ports/k210-freertos/platform/drivers/include/driver_config.h
@@ -3,13 +3,11 @@
 #define __DRIVER_CONFIG_H
 
 ////////// LCD ////////////
-#define LCD_W_MAX 320
-#define LCD_H_MAX 240
+#define LCD_W_MAX 640
+#define LCD_H_MAX 480
 
-#define LCD_W     320
-#define LCD_H     240
+#define LCD_W 320
+#define LCD_H 240
 ///////////////////////////
 
-
 #endif
-
